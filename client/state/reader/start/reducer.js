4a28c47feae2921c255eba687358ea97752237b0
bluefuton <chris@bluefuton.com>
2016-06-07 15:03:18 +0100

    Insert new recommendations at the end of the list

--- client/state/reader/start/reducer.js
+++ client/state/reader/start/reducer.js
@@ -4,10 +4,7 @@
 import { combineReducers } from 'redux';
 import union from 'lodash/union';
 import findIndex from 'lodash/findIndex';
-import forEach from 'lodash/forEach';
 import filter from 'lodash/filter';
-import has from 'lodash/has';
-import debugModule from 'debug';
 
 /**
  * Internal dependencies
@@ -25,11 +22,6 @@ import { itemsSchema } from './schema';
 import { isValidStateWithSchema } from 'state/utils';
 
 /**
- * Module variables
- */
-const debug = debugModule( 'calypso:redux:reader-start-recommendations' );
-
-/**
  * Tracks all known list objects, indexed by list ID.
  *
  * @param  {Array} state  Current state
@@ -39,8 +31,6 @@ const debug = debugModule( 'calypso:redux:reader-start-recommendations' );
 export function items( state = [], action ) {
 	switch ( action.type ) {
 		case READER_START_RECOMMENDATIONS_RECEIVE:
-			let updatedRecommendations = state;
-
 			// Filter out any recommendations we already have
 			const newRecommendations = filter( action.recommendations, ( incomingRecommendation ) => {
 				if ( findIndex( state, ( existingRecommendation ) => {
@@ -51,31 +41,7 @@ export function items( state = [], action ) {
 				return true;
 			} );
 
-			const totalRecommendationsCount = state.length + newRecommendations.length;
-
-			forEach( newRecommendations, ( recommendation ) => {
-				// Find the parent recommendation, if there is one.
-				let parentPosition = findIndex( state, ( item ) => {
-					if ( ! has( item, 'recommended_site_ID' ) ) {
-						return false;
-					}
-					return item.recommended_site_ID === recommendation.origin_site_ID && item.recommended_post_ID === recommendation.origin_post_ID;
-				} );
-
-				let childPosition;
-				if ( parentPosition >= 0 ) {
-					// Insert the new recommendation immediately after the parent, if we found one.
-					childPosition = parentPosition + 1;
-				} else {
-					// No parent recommendation? Insert it at the end.
-					childPosition = totalRecommendationsCount - 1;
-				}
-
-				debug( 'Inserting recommendation ID ' + recommendation.ID + ' at position ' + childPosition );
-
-				updatedRecommendations = insertItemAtArrayPosition( updatedRecommendations, recommendation, childPosition );
-			} );
-			return updatedRecommendations;
+			return state.concat( newRecommendations );
 
 		case SERIALIZE:
 			return state;
@@ -121,12 +87,6 @@ export function recommendationsInteractedWith( state = [], action ) {
 	}
 
 	return state;
-}
-
-function insertItemAtArrayPosition( array, item, position ) {
-	const beforeSlice = array.slice( 0, position );
-	const afterSlice = array.slice( position );
-	return beforeSlice.concat( item, afterSlice );
 }
 
 export default combineReducers( {
